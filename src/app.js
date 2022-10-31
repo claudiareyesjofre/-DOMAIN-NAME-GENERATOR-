a953541bf89f1572af5402945f9ab49f2b700efa
claudiareyesjofre <claudia.reyes.joffre@gmail.com>
2022-10-31 16:06:46 +0000

    domain name generator bucle

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("#the-excuse").innerHTML = domainName();
+
+function domainName() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var num = ["jogger", "racoon"];
+
+  let i = 0;
+  let j = 0;
+  let h = 0;
+
+  let Temp = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let h = 0; h < num.length; h++) {
+        Temp += pronoun[i] + adj[j] + num[h] + ".com <br>";
+      }
+    }
+  }
+  return Temp;
+}
